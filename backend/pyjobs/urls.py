98f03a4606a7ae1aed323b307d4bb457a11f86bc
STI26 <s4i2t6@gmail.com>
2020-12-12 14:05:24 +0300

    added swagger

--- backend/pyjobs/urls.py
+++ backend/pyjobs/urls.py
@@ -1,9 +1,35 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="PyJobs API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="",
+      contact=openapi.Contact(email="admin@mail.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=False,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('api/resumes/', include('resume.urls')),
     path('api/vacancies/', include('vacancy.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0),
+            name='scema-json'),
+    re_path(r'^swagger/$',
+            schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'),
+    re_path(r'^redoc/$',
+            schema_view.with_ui('redoc', cache_timeout=0),
+            name='schema-redoc'),
 ]
