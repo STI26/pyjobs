daf2fd6282482dfb476ac067e003b7b15004fd3c
STI26 <s4i2t6@gmail.com>
2020-12-19 12:43:21 +0300

    Revert "generate random secret key"
    This reverts commit ad818a8dfa1f2b068fd9d1aaee8beb19fd2df64d.

--- backend/configuration.example.py
+++ backend/configuration.example.py
@@ -1,12 +1,4 @@
-import random
-
-def random_key():
-    charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*(-_=+)'
-    secure_random = random.SystemRandom()
-    return ''.join(secure_random.sample(charset, 50))
-
-
-APP_SECRET_KEY = random_key() # or use static secret_key
+APP_SECRET_KEY = 'secret_key'
 APP_DEBUG = True
 
 DATABASE = {
