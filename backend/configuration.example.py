c9c0d6dd33338157e9b04536373d6a5e0f499597
STI26 <s4i2t6@gmail.com>
2020-12-19 12:48:21 +0300

    generate random secret key

--- backend/configuration.example.py
+++ backend/configuration.example.py
@@ -1,13 +1,21 @@
-APP_SECRET_KEY = 'secret_key'
-APP_DEBUG = True
+import random
+
+def random_key():
+    charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*(-_=+)'
+    secure_random = random.SystemRandom()
+    return ''.join(secure_random.sample(charset, 50))
+
+
+APP_SECRET_KEY = random_key() # or use static secret_key
+APP_DEBUG = False
 
 DATABASE = {
     'ENGINE': 'django.db.backends.postgresql',
-    'NAME': '',               # Database name
-    'USER': '',               # PostgreSQL username
-    'PASSWORD': '',           # PostgreSQL password
+    'NAME': 'postgres',       # Database name
+    'USER': 'postgres',       # PostgreSQL username
+    'PASSWORD': 'postgres',   # PostgreSQL password
     'HOST': 'localhost',      # Database server
-    'PORT': '',               # Database port (leave blank for default)
+    'PORT': 5432,             # Database port (leave blank for default)
 }
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
