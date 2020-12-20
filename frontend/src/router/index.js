1f8fb15242f8cdafcee3b0a697392c9554124fe6
STI26 <s4i2t6@gmail.com>
2020-12-20 22:02:52 +0300

    fixed title

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { nextTick } from 'vue'
 import store from '../store'
 import M from 'materialize-css'
 
@@ -12,61 +13,61 @@ const routes = [
   {
     path: '/resume',
     name: 'ResumeList',
-    meta: { layout: 'advanced-layout' },
+    meta: { layout: 'advanced-layout', title: 'Резюме' },
     component: () => import('../views/resume/ResumeList.vue')
   },
   {
     path: '/newresume',
     name: 'ResumeCreate',
-    meta: { layout: 'base-layout', onlyAuth: true },
+    meta: { layout: 'base-layout', onlyAuth: true, title: 'Новое Резюме' },
     component: () => import('../views/resume/ResumeCreate.vue')
   },
   {
     path: '/resume/:id/',
     name: 'ResumeDetail',
-    meta: { layout: 'base-layout' },
+    meta: { layout: 'base-layout', title: 'Просмотр Резюме' },
     component: () => import('../views/resume/ResumeDetail.vue')
   },
   {
     path: '/resume/:id/edit',
     name: 'ResumeEdit',
-    meta: { layout: 'base-layout', onlyAuth: true },
+    meta: { layout: 'base-layout', onlyAuth: true, title: 'Изменить Резюме' },
     component: () => import('../views/resume/ResumeEdit.vue')
   },
   {
     path: '/profile/',
     name: 'ProfileDetail',
-    meta: { layout: 'base-layout', onlyAuth: true },
+    meta: { layout: 'base-layout', onlyAuth: true, title: 'Профиль' },
     component: () => import('../views/profile/ProfileDetail.vue')
   },
   {
     path: '/profile/edit',
     name: 'ProfileEdit',
-    meta: { layout: 'base-layout', onlyAuth: true },
+    meta: { layout: 'base-layout', onlyAuth: true, title: 'Изменить Профиль' },
     component: () => import('../views/profile/ProfileEdit.vue')
   },
   {
     path: '/vacancy',
     name: 'VacancyList',
-    meta: { layout: 'advanced-layout' },
+    meta: { layout: 'advanced-layout', title: 'Вакансии' },
     component: () => import('../views/vacancy/VacancyList.vue')
   },
   {
     path: '/newvacancy',
     name: 'VacancyCreate',
-    meta: { layout: 'base-layout', onlyAuth: true },
+    meta: { layout: 'base-layout', onlyAuth: true, title: 'Новая Вакансия' },
     component: () => import('../views/vacancy/VacancyCreate.vue')
   },
   {
     path: '/vacancy/:id/',
     name: 'VacancyDetail',
-    meta: { layout: 'base-layout' },
+    meta: { layout: 'base-layout', title: 'Просмотр Вакансии' },
     component: () => import('../views/vacancy/VacancyDetail.vue')
   },
   {
     path: '/vacancy/:id/edit',
     name: 'VacancyEdit',
-    meta: { layout: 'base-layout', onlyAuth: true },
+    meta: { layout: 'base-layout', onlyAuth: true, title: 'Изменить Вакансию' },
     component: () => import('../views/vacancy/VacancyEdit.vue')
   },
   {
@@ -91,6 +92,13 @@ router.beforeEach((to, from, next) => {
   } else {
     next()
   }
+})
+
+const DEFAULT_TITLE = 'PyJobs'
+router.afterEach((to, from) => {
+  nextTick(() => {
+    document.title = to.meta.title ? `${DEFAULT_TITLE}: ${to.meta.title}` : DEFAULT_TITLE
+  })
 })
 
 export default router
